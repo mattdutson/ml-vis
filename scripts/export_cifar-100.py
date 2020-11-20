a05972c43ff26a58da3466c4a1ed4cb4893dc61e
Matthew Dutson <mattdutson@icloud.com>
2020-11-20 13:07:59 -0600

    Simplify export_cifar-100.py

--- scripts/export_cifar-100.py
+++ scripts/export_cifar-100.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import argparse
 import os
 import os.path as path
 import shutil
@@ -10,66 +9,46 @@ import tensorflow as tf
 from tensorflow.keras.datasets import cifar100
 
 
-def main(args):
-    def ensure_exists(dirname):
-        if not path.isdir(dirname):
-            os.makedirs(dirname)
+def export_split(x, y_f, y_c, name):
+    split_dirname = path.join('data', name)
+    os.makedirs(split_dirname, exist_ok=True)
 
-    def export_split(x, y_fine, y_coarse, name):
-        split_dirname = path.join(args.out_dir, name)
-        ensure_exists(split_dirname)
-
-        # Write a CSV file with labels
-        csv_filename = path.join(split_dirname, 'labels.csv')
-        with open(csv_filename, 'w') as csv_file:
-            print('Writing CSV file "{}"...'.format(csv_filename))
-            csv_file.write('fine,coarse\n')
-            for i in range(x.shape[0]):
-                csv_file.write('{},{}\n'.format(y_fine[i][0], y_coarse[i][0]))
-            print('Done.')
-
-        # Write a directory of image files
-        image_dirname = path.join(split_dirname, 'images')
-        ensure_exists(image_dirname)
-        print('Writing images to "{}"...'.format(image_dirname))
+    # Write a .csv file with labels
+    csv_filename = path.join(split_dirname, 'labels.csv')
+    with open(csv_filename, 'w') as csv_file:
+        print('Writing CSV file "{}"...'.format(csv_filename))
+        csv_file.write('fine,coarse\n')
         for i in range(x.shape[0]):
-            image_filename = path.join(image_dirname, '{}.png'.format(i))
-            tf.io.write_file(image_filename, tf.image.encode_png(x[i]))
+            csv_file.write('{},{}\n'.format(y_f[i][0], y_c[i][0]))
         print('Done.')
 
-        # Zip the images and delete the directory
-        print('Zipping and deleting "{}"...'.format(image_dirname))
-        shutil.make_archive(image_dirname, 'zip', image_dirname, '.')
-        shutil.rmtree(image_dirname)
-        print('Done.')
+    # Write a directory of image files
+    image_dirname = path.join(split_dirname, 'images')
+    os.makedirs(split_dirname, exist_ok=True)
+    print('Writing images to "{}"...'.format(image_dirname))
+    for i in range(x.shape[0]):
+        image_filename = path.join(image_dirname, '{}.png'.format(i))
+        tf.io.write_file(image_filename, tf.image.encode_png(x[i]))
+    print('Done.')
 
-        # Write a .npz file (more compact and efficient than the above,
-        # but less portable)
-        npz_filename = path.join(split_dirname, 'all.npz')
-        print('Writing NPZ file "{}"...'.format(npz_filename))
-        np.savez_compressed(npz_filename, x=x, y_fine=y_fine, y_coarse=y_coarse)
-        print('Done.')
+    # Zip the images and delete the directory
+    print('Zipping and deleting "{}"...'.format(image_dirname))
+    shutil.make_archive(image_dirname, 'zip', image_dirname, '.')
+    shutil.rmtree(image_dirname)
+    print('Done.')
 
-    # Use TensorFlow to load the data
-    (x_train, y_train_fine), (x_test, y_test_fine) = cifar100.load_data(label_mode='fine')
-    (_, y_train_coarse), (_, y_test_coarse) = cifar100.load_data(label_mode='coarse')
-
-    # Process the train and test splits
-    export_split(x_train, y_train_fine, y_train_coarse, 'train')
-    export_split(x_test, y_test_fine, y_test_coarse, 'test')
+    # Write a .npz file (more compact but less portable)
+    npz_filename = path.join(split_dirname, 'all.npz')
+    print('Writing NPZ file "{}"...'.format(npz_filename))
+    np.savez_compressed(npz_filename, x=x, y_fine=y_f, y_coarse=y_c)
+    print('Done.')
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-        add_help=False)
+# Use TensorFlow to load the data (both fine and coarse labels)
+(x_train, _), (x_test, _) = cifar100.load_data()
+(_, y_train_f), (_, y_test_f) = cifar100.load_data(label_mode='fine')
+(_, y_train_c), (_, y_test_c) = cifar100.load_data(label_mode='coarse')
 
-    parser.add_argument(
-        '-h', '--help', action='help',
-        help='Display this help message and exit.')
-
-    parser.add_argument(
-        '-o', '--out-dir', default='data',
-        help='The directory where the dataset should be exported.')
-
-    main(parser.parse_args())
+# Process the train and test splits
+export_split(x_train, y_train_f, y_train_c, 'train')
+export_split(x_test, y_test_f, y_test_c, 'test')
