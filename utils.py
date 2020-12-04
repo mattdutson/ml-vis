6048ba8fd0ae69ab46990c300c1377a203b9ca6c
Matthew Dutson <mattdutson@icloud.com>
2020-12-04 14:35:33 -0600

    Add class hierarchy information

--- utils.py
+++ utils.py
@@ -1,7 +1,18 @@
 import os.path as path
 
 
+def cifar100_hier():
+    with open(path.join('names', 'hier.csv'), 'r') as hier_file:
+        lines = map(str.strip, hier_file.readlines())
+    hier = {}
+    for i, line in enumerate(lines):
+        items = line.split(',')
+        for j, item in enumerate(items[1:]):
+            hier[item] = items[0]
+    return hier
+
+
 def cifar100_names(label_mode='fine'):
-    names_filename = path.join('names', '{}.txt'.format(label_mode))
+    names_filename = path.join('names', '{}.csv'.format(label_mode))
     with open(names_filename, 'r') as names_file:
         return list(map(str.strip, names_file.readlines()))
