0b4c97f033309d401604304731b871ff0abf9d84
Matthew Dutson <mattdutson@icloud.com>
2020-12-14 20:39:34 -0600

    Update setup.sh

--- service/setup.sh
+++ service/setup.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-sudo cp ml-vis.service /etc/systemd/system/ml-vis.service
+sudo cp service/ml-vis.service /etc/systemd/system/ml-vis.service
 sudo systemctl daemon-reload
 sudo systemctl enable ml-vis.service
-
